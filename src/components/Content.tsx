7b1467f6433afdf38546cdeb72ed542ba29b6a1c
Leandro Almeida <leo.leandro.ars@gmail.com>
2025-11-20 17:11:17 -0300

    Renderiza nós do grafo

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,18 +1,36 @@
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { CircleMarker, MapContainer, TileLayer, Tooltip } from "react-leaflet";
+import { cities } from "../utils/cities";
 
 function Content() {
   return (
-   <div>
-      <MapContainer 
-        center={[ -15.793889, -47.882778 ]}
-        zoom={10} 
+    <div>
+      <MapContainer
+        center={[-15.793889, -47.882778]}
+        zoom={11}
         scrollWheelZoom={false}
         className="h-175"
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
         />
+        {cities.map((city, index) => (
+          <CircleMarker
+            key={index}
+            center={city.coordenadas}
+            radius={4}
+            pathOptions={{ color: "blue", fillColor: "blue", fillOpacity: 0.6 }}
+          >
+            <Tooltip
+              direction="top"
+              offset={[0, -5]}
+              opacity={1}
+              permanent={false}
+            >
+              {city.name}
+            </Tooltip>
+          </CircleMarker>
+        ))}
       </MapContainer>
     </div>
   );
---
+++ src/utils/cities.ts
@@ -0,0 +1,126 @@
+export const cities: { name: string; coordenadas: [number, number] }[] = [
+  {
+    name: "Brasília (Plano Piloto)",
+    coordenadas: [-15.793889, -47.882778],
+  },
+  {
+    name: "Gama",
+    coordenadas: [-16.015278, -48.068611],
+  },
+  {
+    name: "Taguatinga",
+    coordenadas: [-15.835556, -48.056944],
+  },
+  {
+    name: "Brazlândia",
+    coordenadas: [-15.670833, -48.198889],
+  },
+  {
+    name: "Sobradinho",
+    coordenadas: [-15.651389, -47.785833],
+  },
+  {
+    name: "Planaltina",
+    coordenadas: [-15.614167, -47.648611],
+  },
+  {
+    name: "Paranoá",
+    coordenadas: [-15.775278, -47.779444],
+  },
+  {
+    name: "Núcleo Bandeirante",
+    coordenadas: [-15.871389, -47.952778],
+  },
+  {
+    name: "Ceilândia",
+    coordenadas: [-15.822222, -48.112222],
+  },
+  {
+    name: "Guará",
+    coordenadas: [-15.825833, -47.950556],
+  },
+  {
+    name: "Cruzeiro",
+    coordenadas: [-15.788056, -47.929167],
+  },
+  {
+    name: "Samambaia",
+    coordenadas: [-15.874444, -48.078333],
+  },
+  {
+    name: "Santa Maria",
+    coordenadas: [-16.024444, -47.986667],
+  },
+  {
+    name: "São Sebastião",
+    coordenadas: [-15.914167, -47.783611],
+  },
+  {
+    name: "Recanto das Emas",
+    coordenadas: [-15.916944, -48.083611],
+  },
+  {
+    name: "Lago Sul",
+    coordenadas: [-15.848333, -47.870278],
+  },
+  {
+    name: "Riacho Fundo",
+    coordenadas: [-15.874722, -48.009444],
+  },
+  {
+    name: "Lago Norte",
+    coordenadas: [-15.709167, -47.872778],
+  },
+  {
+    name: "Candangolândia",
+    coordenadas: [-15.856944, -47.948611],
+  },
+  {
+    name: "Águas Claras",
+    coordenadas: [-15.851667, -48.026389],
+  },
+  {
+    name: "Riacho Fundo II",
+    coordenadas: [-15.899167, -48.019722],
+  },
+  {
+    name: "Sudoeste/Octogonal",
+    coordenadas: [-15.7975, -47.944722],
+  },
+  {
+    name: "Varjão",
+    coordenadas: [-15.700556, -47.866389],
+  },
+  {
+    name: "Park Way",
+    coordenadas: [-15.900278, -47.964722],
+  },
+  {
+    name: "SCIA/Estrutural",
+    coordenadas: [-15.786944, -47.954444],
+  },
+  {
+    name: "Sobradinho II",
+    coordenadas: [-15.650833, -47.751667],
+  },
+  {
+    name: "Jardim Botânico",
+    coordenadas: [-15.871389, -47.772222],
+  },
+  {
+    name: "Itapoã",
+    coordenadas: [-15.7225, -47.744444],
+  },
+  {
+    name: "SIA",
+    coordenadas: [-15.808889, -47.963056],
+  },
+  {
+    name: "Vicente Pires",
+    coordenadas: [-15.821667, -48.015833],
+  },
+  {
+    name: "Fercal",
+    coordenadas: [-15.623056, -47.814722],
+  },
+];
