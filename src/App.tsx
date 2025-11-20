793b4de9c11b9f779300a7a609807f782604e5c6
Leandro Almeida <leo.leandro.ars@gmail.com>
2025-11-20 16:13:28 -0300

    Renderiza mapa centralizado no Distrito Federal

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import Content from "./components/Content";
-import Header from "./components/Header";
+// import Header from "./components/Header";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <div className="max-w-7xl w-full bg-white shadow-lg rounded-lg p-6 space-y-6">
-        <Header />
+        {/* <Header /> */}
         <Content />
       </div>
     </div>
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,7 +1,19 @@
+import { MapContainer, TileLayer } from 'react-leaflet'
+
 function Content() {
   return (
-    <div className="text-center">
-      <span>Content</span>
+   <div>
+      <MapContainer 
+        center={[ -15.793889, -47.882778 ]}
+        zoom={10} 
+        scrollWheelZoom={false}
+        className="h-175"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
     </div>
   );
 }
